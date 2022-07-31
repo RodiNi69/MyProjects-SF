fd21b6b8fd51696aff9875e7d901b23c56833ab5
RodiNi69 <108999954+RodiNi69@users.noreply.github.com>
2022-07-31 21:18:25 +0300

    Add files via upload

---
+++ С5.6/config.py
@@ -0,0 +1,11 @@
+TOKEN = '5507089978:AAE8FB-VJgd4BUldbwBWC78NEEukTLNMLJA'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'юань': 'CNY',
+    'иена': 'JPY'
+}
